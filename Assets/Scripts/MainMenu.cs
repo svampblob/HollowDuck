0f89c0325bcdd2866fd8db70525e19cffb853fec
Simon Andersson <simon.andersson2@elev.ga.lbs.se>
2019-02-04 09:21:09 +0100

    Fixar
    börjar med ready up meny nu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //Input.GetJoystickNames().Length
+    bool player1;
+    bool player2;
+    bool player3;
+    bool player4;
+
+    public void ReadyUp()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            player1 = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+        {
+            player2 = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Joystick3Button0))
+        {
+            player3 = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Joystick4Button0))
+        {
+            player4 = true;
+        }
+
+    }
+        public void AmountPlayers()
+        {
+        Debug.Log("" + Input.GetJoystickNames().Length);
+        }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
