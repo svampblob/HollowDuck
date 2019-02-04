242580195f4bcd77c1aead376f9030badd1e0ffc
Simon Andersson <simon.andersson2@elev.ga.lbs.se>
2019-02-04 10:48:47 +0100

    jobbigt
    fixar med ready up menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,34 +13,82 @@ public class MainMenu : MonoBehaviour
 
     public void ReadyUp()
     {
+        if (Input.GetJoystickNames().Length == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+            {
+                player1 = true;
+            }
+            if (player1 == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        if (Input.GetJoystickNames().Length == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+            {
+                player1 = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+            {
+                player2 = true;
+            }
+            if (player1 == true && player2 == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
 
-        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
-        {
-            player1 = true;
         }
-        if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+        if (Input.GetJoystickNames().Length == 3)
         {
-            player2 = true;
+            if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+            {
+                player1 = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+            {
+                player2 = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Joystick3Button0))
+            {
+                player3 = true;
+            }
+            if (player1 == true && player2 == true && player3 == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Joystick3Button0))
+        if (Input.GetJoystickNames().Length == 4)
         {
-            player3 = true;
+            if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+            {
+                player1 = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+            {
+                player2 = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Joystick3Button0))
+            {
+                player3 = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Joystick4Button0))
+            {
+                player4 = true;
+            }
+            if (player1 == true && player2 == true && player3 == true && player4 == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Joystick4Button0))
-        {
-            player4 = true;
-        }
-
     }
-        public void AmountPlayers()
-        {
-        Debug.Log("" + Input.GetJoystickNames().Length);
-        }
-
-    public void PlayGame()
+    public void Lobby()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
+
     public void QuitGame()
     {
         Application.Quit();
