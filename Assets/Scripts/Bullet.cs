23a8214d004bd85dbe344262687030508e9edd5f
Simon Andersson <simon.andersson2@elev.ga.lbs.se>
2019-04-09 11:38:19 +0200

    Börjar mer med killcountern

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
-
+public class Bullet : MonoBehaviour
+{
+    public int Firer;
     public float speed = 20f;
     public Rigidbody2D Rb;
     public int damage = 1;
     public Movment move;
     public string fire;
     public int Player;
+    public Transform shooter;
+    public int score = 1;
+
 
     // Use this for initialization
     void Start()
     {
-        Rb.velocity = transform.right* speed;
-        
-        if(Input.GetAxisRaw(move.movekey + move.Player) < 0)
+
+        Rb.velocity = transform.right * speed;
+
+        if (Input.GetAxisRaw(move.movekey + move.Player) < 0)
         {
             speed = -speed;
         }
@@ -24,17 +29,35 @@ public class Bullet : MonoBehaviour {
         {
             speed = 20f;
         }
+        GetComponent<Bullet>().shooter = transform;
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          //  Debug.Log("[0]", score);
+            Debug.Log("oh no");
+        }
     }
 
-     void OnTriggerEnter2D(Collider2D hitInfo)
-     {
+
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
         Player player = hitInfo.GetComponent<Player>();
         if (player != null)
         {
-        
+
             player.TakeDamage(damage);
             Destroy(gameObject);
+            shooter.SendMessage("AddToScore", 5);
         }
-        
     }
+    void AddToScore(int points)
+    {
+        score += points;
+    }
+
+
 }
