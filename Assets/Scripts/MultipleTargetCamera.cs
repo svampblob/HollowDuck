7c93cd3d8723d0078cda9becbdff8d69beb2d4ec
Simon Andersson <simon.andersson2@elev.ga.lbs.se>
2019-05-20 07:11:05 +0200

    Lyckades inte lösa kamera problemet som jag satt på hela veckan men funkar normalt verkar det som om det bara spawnar in spelare 1 och 2

--- Assets/Scripts/MultipleTargetCamera.cs
+++ Assets/Scripts/MultipleTargetCamera.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Camera))]
 public class MultipleTargetCamera : MonoBehaviour
 {
-    
+
     public List<Transform> targets;
     public Vector3 offset;
     public float smoothTime = 0.2f;
@@ -13,49 +14,88 @@ public class MultipleTargetCamera : MonoBehaviour
     public float zoomLimiter = 10f;
     private Vector3 velocity;
     private Camera cam;
+    public bool one;
+    public bool two;
+    public bool three;
+    public bool four;
 
+    PlayersInGame playersInGame;
 
     public Camera maincam;
     public float shakeAmount = 0;
 
     void Start()
     {
+        targets.Clear();
         cam = GetComponent<Camera>();
+        Targets();
+    }
+
+   void Targets()
+    {
+
+        if (GameObject.FindGameObjectWithTag("Player1").transform != null && one == false)
+        {
+            print("Hello");
+            targets.Add(GameObject.FindGameObjectWithTag("Player1").transform);
+            one = true;
+        }
+        if (GameObject.FindGameObjectWithTag("Player2").transform != null && two == false)
+        {
+            targets.Add(GameObject.FindGameObjectWithTag("Player2").transform);
+            two = true;
+        }
+        if (playersInGame.Player3 != null)
+        {
+            print("Player3 found");
+            targets.Add(playersInGame.Player3.transform);
+            three = true;
+        }
+        if (GameObject.FindGameObjectWithTag("Player4").transform != null && four == false)
+        {
+            targets.Add(GameObject.FindGameObjectWithTag("Player4").transform);
+            four = true;
+        }
+    }
+    private void Update()
+    {
+        print("players active" + targets.Count);
     }
 
     private void LateUpdate()
-    { 
+    {
         if (targets.Count == 0)
             return;
 
         Move();
-        Zoom(); 
+        Zoom();
     }
 
 
     void Zoom()
     {
         float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        // print (GetGreatestDistance() / zoomLimiter);
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom, Time.deltaTime);
         Vector2 Left = targets[0].transform.position;
-            Vector2 Right = targets[0].transform.position;
-        if(targets.Count > 1)
+        Vector2 Right = targets[0].transform.position;
+        if (targets.Count > 1)
         {
 
             for (int i = 0; i < targets.Count; i++)
             {
-                if(targets[i].transform.position.x < Left.x)
+                if (targets[i].transform.position.x < Left.x)
                 {
                     Left = targets[i].transform.position;
                 }
-                if(targets[i].transform.position.x > Right.x)
+                if (targets[i].transform.position.x > Right.x)
                 {
                     Right = targets[i].transform.position;
                 }
             }
 
         }
-        float dist =  Vector2.Distance(Left, Right);
+        float dist = Vector2.Distance(Left, Right);
     }
     void Move()
     {
@@ -81,14 +121,24 @@ public class MultipleTargetCamera : MonoBehaviour
         {
             return targets[0].position;
         }
+
+
         var bounds = new Bounds(targets[0].position, Vector3.zero);
+
         for (int i = 0; i < targets.Count; i++)
         {
+
+
             bounds.Encapsulate(targets[i].position);
+
+
+
         }
 
-
         return bounds.center;
+
+
+
     }
     void Awake()
     {
